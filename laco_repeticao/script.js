a4f74e49cdd7d2e68f0b8cb0b0c7cc36f4df9a8a
ValeriaMary <valeriamary.vms@gmail.com>
2022-08-05 21:21:37 -0300

    update

---
+++ laco_repeticao/script.js
@@ -0,0 +1,14 @@
+
+//Tabuada multiplicação
+function multiplicar(){
+    for (var y = 1; y <= 10; y++){
+    
+    for(var x = 1; x <= 10; x++){
+        document.write(y + 'x' + x + '=' + (y * x) + '<br />')
+    }
+    document.write('<hr />')
+}
+<div>
+<button class="btn btn-light" type="submit"><a href="tabuada.html">Voltar</a></button>
+</div>
+}
